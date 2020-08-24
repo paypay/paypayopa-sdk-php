224e35e697368331d0796f8fd6d6a0a83f56ea1d
Ayaskant Mishra <t-ayaskant.mishra@paypay-corp.co.jp>
2020-08-24 16:43:10 +0530

    Fixed config file issue with tests. Spaces in config

--- tests/config.php
+++ tests/config.php
@@ -2,7 +2,7 @@
     $config = [
         'key'=>'API_KEY',
         'secret'=>'API_SECRET',
-        'uaid'=>'<USER AUTHORIZATION IDENTIFIER>',
+        'uaid'=>'<USER_AUTHORIZATION_IDENTIFIER>',
         'mid' => '<MERCHASNT_ID>',
         'uaresponse'=>"<USER_AUTH_RESPONSE_JWT>"
     ];
