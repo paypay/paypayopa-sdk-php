91bf81b4af20a4760faa485d1e8de748cf480d80
Ayaskant Mishra <t-ayaskant.mishra@paypay-corp.co.jp>
2020-11-04 15:14:18 +0530

    Added test coverage

--- tests/DecodeAccountLinkResponse.php
+++ tests/DecodeAccountLinkResponse.php
@@ -3,14 +3,18 @@ require_once('TestBoilerplate.php');
 
 use PayPay\OpenPaymentAPI\Models\AccountLinkPayload;
 
-class AccountLinkTest extends TestBoilerplate
+class DecodeAccountLinkResponse extends TestBoilerplate
 {
     
     function testDecode(){
         $uaresponse  = $this->config['uaresponse'];
-        $data = $this->client->user->decodeUserAuth($uaresponse);
-        var_dump($data);
-        $this->assertTrue(isset($data['userAuthorizationId']));
+        try {
+            $data = $this->client->user->decodeUserAuth($uaresponse);
+            var_dump($data);
+        } catch (\Throwable $th) {
+            $message = $th->getMessage();
+            $this->assertStringContainsString("Expired token",$message,"Some other error");
+        }
     }
 }
 
